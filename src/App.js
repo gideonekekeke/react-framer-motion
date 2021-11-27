7c7d4c1f0d32e100a049ace2cd07d822ba258057
gideonekekeke <Gideonekeke64@gmail.com>
2021-11-27 05:12:48 -0800

    framermotion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { motion } from "framer-motion";
+import "./App.css";
+import styled from "styled-components";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<div>
+					<motion.h1
+						transition={{
+							delay: 0.5,
+							duration: 5,
+							type: "spring",
+							stiffness: 300,
+						}}
+						animate={{ scale: 1.1, x: 0, opacity: 1 }}
+						initial={{ x: "-100%", opacity: 0 }}>
+						Welcome Gideon
+					</motion.h1>
+					<ButtonHold whileHover={{ scale: 1.1 }}>Framer button</ButtonHold>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
+
+const ButtonHold = styled(motion.div)`
+	height: 70px;
+	background: coral;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 10px;
+	cursor: pointer;
+`;
